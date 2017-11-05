943648c190b9de4650153b849f5c00b3d662ba92
theis <theis9953>
2017-11-05 12:21:21 +0100

    adding note-stuff to WSL

---
+++ SubProject_2/Controllers/NoteController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using DAL;
+using WebService.Models;
+
+
+
+namespace WebService
+{
+    [Route("/api/favorite/note")]
+    public class NoteController : PageController
+    {
+
+        private readonly IDataService _dataService;
+        private readonly IMapper _mapper;
+
+
+        public NoteController(IDataService dataService, IMapper mapper)
+        {
+            _dataService = dataService;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{favId}", Name = nameof(GetNote))]
+        public IActionResult GetNote(int favId)
+        {
+            var note = _dataService.GetNote(favId);
+            if (note == null) return NotFound();
+
+            var model = _mapper.Map<NoteModel>(note);
+            model.Url = Url.Link(nameof(GetNote), new { favId = note.favorite_id });
+
+            return Ok(model);
+        }
+
+        //[HttpPost{id}", Name = nameof(CreateNote))]
+        //public IActionResult CreateNote(int favId, string body)
+        //{
+        //    var note = _dataService.CreateNote(favId, body);
+        //    if (note == null) return NotFound();
+
+        //    var model = _mapper.Map<NoteModel>(note);
+
+        //    return CreatedResult()
+        //}
+
+
+
+    }
+}
---
+++ SubProject_2/Models/NoteModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebService.Models
+{
+    public class NoteModel
+    {
+        public string Url { get; set; }
+
+        public int favorite_id { get; set; }
+
+        public DateTime created_timestamp { get; set; }
+
+        public string body { get; set; }
+    }
+}

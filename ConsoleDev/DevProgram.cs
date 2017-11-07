aa1c4c181f8c37194e8541a2c5b6313ecbbfc92e
Jacob H. Clausen <hijakker123@hotmail.com>
2017-11-07 13:37:41 +0100

    Fixing proper linq call from Dataservice in create note method. Not Select, instead Where

--- ConsoleDev/DevProgram.cs
+++ ConsoleDev/DevProgram.cs
@@ -8,7 +8,18 @@ namespace ConsoleDev
         static void Main(string[] args)
         {
             //AnswerTest();
-            QuestionTest();
+            //QuestionTest();
+            var service = new DataService();
+
+            // Create favorite to ensure a favorite exists.
+            var favorite = service.CreateFavorite(19);
+
+            var exampleBody = "this is a note test";
+
+            var createdNote = service.CreateNote(favorite.favorite_id, exampleBody);
+
+            Console.WriteLine(createdNote.favorite_id);
+            Console.WriteLine(createdNote.body);
         }
 
         static void QuestionTest()
--- SOVATestSuite/DALTest.cs
+++ SOVATestSuite/DALTest.cs
@@ -6,6 +6,7 @@ namespace SOVATestSuite
 {
     public class DALTest
     {
+        // Read-only Get tests of posts
         [Fact]
         public void GetPost_ValidId_ReturnsPostObjectWithProperID()
         {
@@ -13,5 +14,28 @@ namespace SOVATestSuite
             var post = service.GetPost(71); // we know from the databse, that 71 is a valid id.
             Assert.Equal(71, post.post_id);
         }
+
+
+        // Create tests of Notes
+        [Fact]
+        public void CreateNote_ValidObject_ReturnsNoteObject()
+        {
+            var service = new DataService();
+
+            // Create favorite to ensure a favorite exists.
+            var favorite = service.CreateFavorite(19);
+
+            var exampleBody = "this is a note test";
+
+            var createdNote = service.CreateNote(favorite.favorite_id, exampleBody);
+
+            Assert.Equal(favorite.favorite_id, createdNote.favorite_id);
+            Assert.Equal(exampleBody, createdNote.body);
+            Assert.NotNull(createdNote.created_timestamp);
+
+            // Cleanup from creating favorite
+            service.DeleteFavorite(favorite.favorite_id);
+        }
+
     }
 }

a1f2f45f7b206e3643096e8cf6f7e2a3eb47d23e
Joey Maagaard King <jmaagaardking@gmail.com>
2017-11-23 20:21:32 +0100

    CoOrcorruingWordModel

---
+++ SubProject_2/Models/CoOrcorruingWordListModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebService.Models
+{
+    public class CoOrcorruingWordListModel
+    {
+        public string co_term { get; set; }
+
+        public int score { get; set; }
+    }
+}

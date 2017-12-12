c5ceae258db0d94511e8bddab4ebb563b555de27
theis <theis9953>
2017-12-12 16:27:56 +0100

    simple validation på front-page

--- SubProject_2/wwwroot/js/components/frontPage/front-page.js
+++ SubProject_2/wwwroot/js/components/frontPage/front-page.js
@@ -4,8 +4,17 @@
         var fpSearchString = ko.observable("");
 
         var searched = function() {
-            bc.publish(bc.events.changeView, { name: "all-posts", fp_msg: fpSearchString()} );
-            bc.publish(bc.events.changeData, { search_string: fpSearchString() });
+
+            console.log(fpSearchString());
+
+
+            if (fpSearchString().length < 1 || fpSearchString().length > 25) {
+                console.log("field not filled out fucko");
+                alert("The typed search string is either too long or too short, which one is it?");
+            } else {
+                bc.publish(bc.events.changeView, { name: "all-posts", fp_msg: fpSearchString()} );
+                bc.publish(bc.events.changeData, { search_string: fpSearchString() });
+            }
         }
 
         return {
@@ -14,4 +23,5 @@
         };
 
     }
+
 });
